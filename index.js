9181a0a460047904c8e83df85ea8479d1fbaa247
Carina B. Prates de Noronha <113940626+carinanoronha@users.noreply.github.com>
2025-08-15 10:15:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 80) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
+    $(window).scroll(function () {
+        if (this.scrollY > 80) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
 
-  /* slide-up */
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+    });
 
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-  });
+    $(".topnav").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".topnav i").toggleClass("active");
+    });
+    $(".navbar .menu li a").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".topnav i").toggleClass("active");
+    });
 
-  /* Toggle Menu/ NavBar */
+    // Typed.js para animação de texto
+    var typed = new Typed(".typing", {
+        strings: ["Front-end Developer", "UI/UX Designer", "Web Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-  $(".topnav").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".topnav i").toggleClass("active");
-  });
-  $(".navbar .menu li a").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".topnav i").toggleClass("active");
-  });
-
-  /* OWL CAROUSEL */
-  
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 1200,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
+    // Owl Carousel
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: { items: 1, nav: false },
+            600: { items: 2, nav: false },
+            1000: { items: 3, nav: false }
+        }
+    });
 });
